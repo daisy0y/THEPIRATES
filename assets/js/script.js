2324e12a7128a521d433ed0e16ad2d0efa4166bd
daisy0y <61371367+daisy0y@users.noreply.github.com>
2020-10-10 04:56:48 +0900

    201010 update header , main-content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,9 @@
 // Json URL
-// const bannerDataUrl = './assets/js/bannerData.json'
+const bannerDataUrl = './assets/js/bannerData.json'
 // const demo_dataUrl = './assets/js/demo_data.json'
 // const itemDataUrl = './assets/js/itemData.json'
-// const storeDataUrl = '/.assets/js/storeData.json'
+const storeDataUrl = './assets/js/storeData.json'
 // const zoneDataUrl = './assets/js/zoneData.json'
-
-
-// fetch(bannerDataUrl)
-//     .then(response => response.json())
-//     .then(function(bannerData){
-//         const banner = bannerData;
-      
-//     })
-// console.log(banner)
-// `<a href="https://www.tpirates.com/${permalink}">${label} <span class="price-color">${price}</span> ${comment}</a>`
-
 
 
 // header-nav toggle
@@ -28,22 +17,96 @@ header.click(function(e){
 
 });
 // banner
-// const data = require('./assets/js/bannerData.json');
-// console.log(data);
-$.getJSON("./assets/js/bannerData.json", function(bannerData) {
-    
-    // $(".container .content .head .rolling-banner ul").append(`<li><a href="https://www.tpirates.com/${bannerData[0].permalink}">${bannerData[0].label}<span class="price-color">${bannerData[0].price}</span>${bannerData[0].comment}</a></li>`);
- 
 
-//     const li = document.createElement("li");
-//    const test = document.getElementById("banner-items");
-//    test.appendChild(li.innerHTML='hello');
-   
+$.getJSON(bannerDataUrl, function(bannerData) {
     for(let i = 0 ; i < bannerData.length ; i ++){
-        $(".container .content .head .rolling-banner ul").append(`<li><a href="https://www.tpirates.com/${bannerData[i].permalink}">${bannerData[i].label} <span class="price-color">${bannerData[i].price}</span> (${bannerData[i].comment})</a></li>`);
-  }
-
-    console.log(bannerData); // this will show the info it in firebug console
-
+        $(".container .content .head .rolling-banner ul").append(`<li class="banner-items"><a href="https://www.tpirates.com/${bannerData[i].permalink}">${bannerData[i].label} <span class="price-color">${bannerData[i].price}</span> (${bannerData[i].comment})</a></li>`);
+    }
 });
 
+// btn Arrow
+const btnArrow = document.querySelector(".btn-arrow");
+const widget = document.getElementById("widget");
+
+function arrowToggle(){
+btnArrow.addEventListener('click',btnClick)
+
+}
+arrowToggle();
+
+
+function btnClick(){
+    if(btnArrow.className === 'btn-arrow btn-down'){
+        btnArrow.classList.remove('btn-down')
+        btnArrow.classList.add('btn-up')
+        widget.classList.add('full')
+        widget.classList.remove('rolling')
+    }else{
+        btnArrow.classList.remove('btn-up')
+        btnArrow.classList.add('btn-down')
+        widget.classList.remove('full')
+        widget.classList.add('rolling')
+    }
+}
+
+// store
+$.getJSON(storeDataUrl, function(storeData) {
+    if($(document).height() ==  $(window).scrollTop() + $(window).height()){
+    for(let i = 0 ; i < 10 ; i ++){
+        $("#wrap .container #main-content .main-title").append(`
+        <section class="card-wrap">
+        <a href="https://www.tpirates.com${storeData[i].uri}">
+            <div class="card-top">
+                <div class="top-left">
+                    <h2>${storeData[i].label}<span>${storeData[i].market}</span></h2>
+                    <p>${storeData[i].description}</p>
+                </div>
+                <div class="top-right">
+                    <strong>★ ${storeData[i].summary.rating}</strong>
+                    <span>영업 중</span>
+                </div>
+            </div>
+            <div class="card-bottom">
+                <span class="thumbnail">
+                    <img src="${storeData[i].thumbnail}" alt="thumbnail">
+                </span>
+            </div>
+        </a>
+    </section>
+        `)}
+      }
+    
+$(window).scroll(function() {
+    if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+        for(let i = 0 ; i < storeData.length ; i ++){
+            if(i < storeData.length){
+            $("#wrap .container #main-content .main-title").append(`
+            <section class="card-wrap">
+            <a href="https://www.tpirates.com${storeData[i].uri}">
+                <div class="card-top">
+                    <div class="top-left">
+                        <h2>${storeData[i].label}<span>${storeData[i].market}</span></h2>
+                        <p>${storeData[i].description}</p>
+                    </div>
+                    <div class="top-right">
+                        <strong>★ ${storeData[i].summary.rating}</strong>
+                        <span>영업 중</span>
+                    </div>
+                </div>
+                <div class="card-bottom">
+                    <span class="thumbnail">
+                        <img src="${storeData[i].thumbnail}" alt="thumbnail">
+                    </span>
+                </div>
+            </a>
+        </section>
+            `);}
+          }
+        
+    }
+}); 
+});
+// store scroll
+
+const test = document.querySelectorAll(".main-title");
+console.log(test);
