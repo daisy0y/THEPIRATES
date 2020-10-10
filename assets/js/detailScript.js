b2b292d4fa104280327bee3ff1646a1192fb2299
daisy0y <61371367+daisy0y@users.noreply.github.com>
2020-10-10 19:11:03 +0900

    201010 update 가게상세페이지

---
+++ assets/js/detailScript.js
@@ -0,0 +1,9 @@
+// buisness time hide
+  $(document).ready(function(){
+    $(".store-detail .time-switch").click(function(){
+      $(".store-detail .bussiness-time.hide").removeClass('hide');
+      $('.store-detail .time-switch').addClass('hide');
+    });
+  });
+
+  
